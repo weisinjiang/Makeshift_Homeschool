4b56993e963e072f7702a083fcafd5101ff59594
weisinjiang <54108149+weisinjiang@users.noreply.github.com>
2020-07-01 17:50:16 -0400

    Update my_bottom_navigation.dart

--- makeshift_homeschool_app/lib/shared/my_bottom_navigation.dart
+++ makeshift_homeschool_app/lib/shared/my_bottom_navigation.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 /// Bottom Navigation buttons that will persist throughout the app
 
-class MyBottomNavigationBar extends StatelessWidget {
+class MyBottomNavigationBar extends StatefulWidget {
+  @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  int currentPageIndex = 0; // tracks the current selected page and highlight it
+
+  // Change the index when a new page is selected
+  void switchPage(int index) {
+    setState(() {
+      currentPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      
+      backgroundColor: Colors.black,
+      iconSize: 20,
+      selectedItemColor: Colors.greenAccent,
+      currentIndex: currentPageIndex,
       items: [
         BottomNavigationBarItem(
-          title: Text(" "),
-          icon: Icon(FontAwesomeIcons.home)
-        ),
+            title: Text("Feed"),
+            icon: Icon(FontAwesomeIcons.home),
+            backgroundColor: Colors.black),
         BottomNavigationBarItem(
-          title: Text(" "),
-          icon: Icon(FontAwesomeIcons.book)
-        ),
+            title: Text("Post"), icon: Icon(FontAwesomeIcons.pencilAlt)),
         BottomNavigationBarItem(
-          title: Text(" "),
-          icon: Icon(FontAwesomeIcons.plus)
-        ),
+            title: Text("Notifications"), icon: Icon(FontAwesomeIcons.bell)),
         BottomNavigationBarItem(
-          title: Text(" "),
-          icon: Icon(FontAwesomeIcons.plus)
-        ),
-        
+            title: Text("Profile"), icon: Icon(FontAwesomeIcons.user)),
       ].toList(),
-
+      onTap: switchPage,
     );
   }
 }
