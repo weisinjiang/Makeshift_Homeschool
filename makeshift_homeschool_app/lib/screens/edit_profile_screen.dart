2da9f12a539a3da09d7e84d2dedebd5c9ea7a56a
weisinjiang <54108149+weisinjiang@users.noreply.github.com>
2020-07-09 15:27:47 -0400

    preset profile images added

--- makeshift_homeschool_app/lib/screens/edit_profile_screen.dart
+++ makeshift_homeschool_app/lib/screens/edit_profile_screen.dart
@@ -1,10 +1,18 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:makeshift_homeschool_app/shared/constants.dart';
 import 'package:provider/provider.dart';
 import '../services/auth.dart';
 import 'package:image_picker/image_picker.dart'; // load images
+
+/// This screen deals with the popup that appears when a user
+/// wants to edit their profile information:
+///   1. Profile Picture
+///   2. Username
+///   3. Bio
+/// It looks for any updates and updates it on Firestore.
 
 class EditProfileScreen extends StatefulWidget {
   final currentData; // Pass in a snapshot of the current data when this edit screen is pushed to the stack
@@ -16,20 +24,18 @@ class EditProfileScreen extends StatefulWidget {
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey();
-  File _imageFile = null;
-  String _newUserName;
-  String _newBio;
-  bool updateProfileInfo = false;
-  // Update data as key value pairs
+  File _imageFile = null; // When users select an image file
+  bool updateProfileInfo = false; // Check if name/bio is changed
 
   /// **************************************************************************
-  /// Build UI Method                                                          *
+  /// Build UI Method
   ///***************************************************************************
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    var auth = Provider.of<AuthProvider>(context); // Used to call Auth method
+    var auth = Provider.of<AuthProvider>(
+        context); // Used to call Auth method above the widget tree
 
     // Stores current data, but updates if there is a change
     Map<String, String> toUpdateData = {
@@ -37,29 +43,79 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       "bio": widget.currentData["bio"]
     };
 
-    // Pick an image by source: Gallery or Camera
-    Future<void> _chooseNewProfilePicture(ImageSource source) async {
+    // Preset Profile Images for user to select from
+    List<Image> preSetProfileImages = [
+      Image.asset("asset/profile_images/bird.png"),
+      Image.asset("asset/profile_images/cat.png"),
+      Image.asset("asset/profile_images/dog.png"),
+      Image.asset("asset/profile_images/frog.png"),
+      Image.asset("asset/profile_images/lion.png")
+    ];
+
+    /// ************************************************************************
+    ///   Method
+    ///   User picks an image from their library or camra
+    ///   @param - source is where the user wants to pick their profile image
+    ///*************************************************************************
+
+    Future<void> _chooseNewProfileImageFromSource(ImageSource source) async {
       ImagePicker imagePicker = ImagePicker();
       final pickedImage = await imagePicker.getImage(
-          source: source, maxHeight: 180, maxWidth: 180);
+          // ask for permission
+          source: source,
+          maxHeight: 180,
+          maxWidth: 180);
       setState(() {
-        _imageFile = File(pickedImage.path);
+        _imageFile = File(pickedImage.path); // set the image path
       });
-      await auth.uploadProfileImage(_imageFile);
+      await auth.uploadProfileImage(_imageFile); // upload to Firestore
     }
 
-    //Remove Image
-    void _clearNewProfilePictureSelection() {
-      setState(() {
-        _imageFile = null;
-      });
+    Widget profileImageList() {
+      return Container(
+        height: screenSize.height * 50,
+        width: screenSize.width,
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: preSetProfileImages.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 110,
+              width: 110,
+              child: preSetProfileImages[index],
+            );
+          },
+        ),
+      );
     }
 
-    // When users want to change their profile picture, they can pick from
-    // library or camera
+    void _showProfileImagePopupSelector() {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Select an Image"),
+              content: profileImageList(),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Save"))
+              ],
+            );
+          });
+    }
+
+    /// ************************************************************************
+    ///   Widget
+    ///   Prompts the user if they want to get their profie image from their
+    ///   camera or library
+    ///   @param - current widget tree context so it can show on top of it
+    ///*************************************************************************
     void _buildImagePickerPopUpMenu(context) {
       showModalBottomSheet(
-          context: context,
+          context: context, // context of the widget this prompt is showing on
           builder: (BuildContext contx) {
             return Container(
               child: Wrap(
@@ -67,7 +123,14 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   ListTile(
                     title: Text("Choose from Library"),
                     onTap: () {
-                      _chooseNewProfilePicture(ImageSource.gallery);
+                      _chooseNewProfileImageFromSource(ImageSource.gallery);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Update Profile Picture"),
+                    onTap: () {
+                      _showProfileImagePopupSelector();
                       Navigator.of(context).pop();
                     },
                   ),
@@ -77,6 +140,10 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
           });
     }
 
+    /// ************************************************************************
+    ///   Widget
+    ///   Main UI that shows on the screen
+    ///*************************************************************************
     return Scaffold(
       // appBar: AppBar(
       //   title: Text("Edit Profile"),
@@ -93,6 +160,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 child: Center(
                     child: CircleAvatar(
                         radius: 50,
+                        // if a new image is selected, show it, otherwise current
                         backgroundImage: _imageFile != null
                             ? FileImage(_imageFile)
                             : NetworkImage(widget.currentData["photoURL"]),
@@ -137,7 +205,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
-                      initialValue: widget.currentData["bio"], //! Change
+                      initialValue: widget.currentData["bio"],
                       decoration: InputDecoration(
                         labelText: "Bio",
                         border: OutlineInputBorder(
