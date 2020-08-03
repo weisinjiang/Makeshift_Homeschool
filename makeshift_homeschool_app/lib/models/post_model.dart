bf7d3a3939447f1d1b7851dde0d16b06ddf8f444
weisinjiang <54108149+weisinjiang@users.noreply.github.com>
2020-08-03 13:07:53 -0400

    moved widget list construction to Post model instead of doing it in the build method in post_expanded.dart

--- makeshift_homeschool_app/lib/models/post_model.dart
+++ makeshift_homeschool_app/lib/models/post_model.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:makeshift_homeschool_app/widgets/post_widgets.dart';
+
 class Post {
   int likes; // Database stored as a string, needs to convert
   String _imageUrl;
@@ -27,8 +30,6 @@ class Post {
   String get getOwnerUid => this._ownerUid;
   Map<String, dynamic> get getPostContents => this._postContents;
 
-
-
   //Setters
   set setLikes(int likes) => this.likes = likes;
   set setTitle(String title) => this._title = title;
@@ -39,4 +40,36 @@ class Post {
   set setPostContents(Map<String, dynamic> contents) =>
       this._postContents = contents;
 
+  /// Convert the contents into a Widget List that can be displayed on the screen
+  List<Widget> getPostWidgetList(Size screenSize) {
+    List<Widget> contentToShowOnScreen = [];
+
+    /// Map<String, Map<String, String>> from database
+    var postContentMap = this._postContents;
+    var numBodies = postContentMap.length; // number of keys in the map
+
+    contentToShowOnScreen.add(buildImage(
+        this._imageUrl, screenSize.height, screenSize.width)); //add image last
+    /// Loop through each body. Body begins with 1
+    for (var body = 0; body < postContentMap.length; body++) {
+      var bodyNum = body + 1; // body1, body2, etc...
+      /// {paragraph1 : text, paragraph2: text, subtitle : text}
+      var bodyContent = postContentMap["body" + bodyNum.toString()];
+
+      /// Add the subtitle first for each body contents
+      var subtitleText = bodyContent["subtitle"];
+      contentToShowOnScreen.add(
+          buildSubTitle(subtitleText, screenSize.width, screenSize.height));
+
+      /// Then add the paragraphs, bodyContent-1 because subtitle is not counted
+      for (var paragraph = 0; paragraph < bodyContent.length - 1; paragraph++) {
+        var paragraphNum = paragraph + 1;
+        var paragraphText = bodyContent["paragraph" + paragraphNum.toString()];
+        contentToShowOnScreen
+            .add(buildParagraph(paragraphText, screenSize.width));
+      }
+    }
+
+    return contentToShowOnScreen;
+  }
 }
