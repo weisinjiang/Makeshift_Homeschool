cd6043b1529459a25058f2aeebea828a57062ba4
sumay11 <69305154+sumay11@users.noreply.github.com>
2020-11-24 11:17:01 -0500

    Added activity to activity page

---
+++ makeshift_homeschool_app/lib/Activities/paperhat.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PaperHat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(  
+        title: Text("Paper hat", style: TextStyle(color: Colors.white),),
+        backgroundColor: Color(0xFF192A56),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView(  
+        children: [
+          Text("Today we're going to make a paper hat! You are going to need a regular piece of paper.\n "),
+          Image.asset("asset/activities/paperhat1.png"),
+          Text(" \nNow, take your piece of paper and fold it in half.\n "),
+          Image.asset("asset/activities/paperhat2.png"),
+          Text(" \nNext, fold the corners into the middle until they touch.\n "),
+          Image.asset("asset/activities/paperhat3.png"),
+          Text(" \nThen, fold one of the bottom flaps up.\n "),
+          Image.asset("asset/activities/paperhat4.png"),
+          Text(" \nAnd lastly, turn it and fold the other flap up the other direction.\n "),
+          Image.asset("asset/activities/paperhat5.png"),
+          Text(" \nNow you are done with your paper hat!\n "),
+          Image.asset("asset/activities/paperhatdone.png"),
+        ],
+      ),
+      )
+    );
+  }
+}
--- makeshift_homeschool_app/lib/screens/activities.dart
+++ makeshift_homeschool_app/lib/screens/activities.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:makeshift_homeschool_app/Activities/paperhat.dart';
 import 'package:makeshift_homeschool_app/Activities/tictactoe.dart';
 
 class Activities extends StatefulWidget {
@@ -8,41 +9,49 @@ class Activities extends StatefulWidget {
 
 class _ActivitiesState extends State<Activities> {
   @override
-  Widget activity(String activity) {
-    return Container(
-        padding: EdgeInsets.all(10),
-        child: Card(
-          color: Colors.grey[300],
-          child: InkWell(
-            child: Center(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                child: Text("${activity}", style: TextStyle(fontSize: 25)),
-              ),
-            ),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TicTacToe()));
-            },
-          ),
-        ));
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar(  
         backgroundColor: Colors.brown[400],
         title: Text("Activities!"),
       ),
-      body: Container(
-        color: Colors.brown[200],
-        child: ListView(
-          children: [
-            activity("tic tac toe"),
-          ],
-        ),
-      ),
-    );
+        body: Container(
+          color: Colors.brown[200],
+          child: ListView(children: [
+      Container(
+          padding: EdgeInsets.all(10),
+          child: Card(
+            color: Colors.grey[300],
+            child: InkWell(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text("Tic Tac Toe", style: TextStyle(fontSize: 25)),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TicTacToe()));
+              },
+            ),
+          )),
+          Container(
+          padding: EdgeInsets.all(10),
+          child: Card(
+            color: Colors.grey[300],
+            child: InkWell(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text("How to make a paper hat", style: TextStyle(fontSize: 25)),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PaperHat()));
+              },
+            ),
+          ))
+    ])));
   }
 }
