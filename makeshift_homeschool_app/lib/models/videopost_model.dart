3b0eaf976d48fec8bef2d2acaa2d1158396cef34
weisinjiang <54108149+weisinjiang@users.noreply.github.com>
2021-02-08 11:50:26 -0500

    Update videopost_model.dart

--- makeshift_homeschool_app/lib/models/videopost_model.dart
+++ makeshift_homeschool_app/lib/models/videopost_model.dart
@@ -3,5 +3,39 @@ import 'package:flutter/material.dart';
 /// Video Object for video posts
 
 class VideoPost with ChangeNotifier {
-  // TODO: Add Instance Var, Constructors, Methods
+  int views; 
+  String link;
+  String createdOn;
+  String title;
+  String owner;
+  String ownerEmail;
+  String postID;
+  bool isLiked;
+
+
+  VideoPost({
+    this.views, this.createdOn, this.link, this.owner, this.ownerEmail, this.title, this.isLiked = false, this.postID}
+  );
+
+  // Getters
+  int get getViews => this.views;
+  String get getLink => this.link;
+  String get getCreatedOn => this.createdOn;
+  String get getTitle => this.title;
+  String get getOwnerEmail => this.ownerEmail;
+  String get getOwner => this.owner;
+  String get getPostID => this.postID;
+
+  // Increment view count
+  Future<void> incrementPostViewCount() async {
+    throw UnimplementedError();
+  }
+
+  // Add this video to the user's bookmark list
+  // uid -> the user's id on firebase
+  Future<void> toggleBookmarkButton(String uid) async {
+    throw UnimplementedError();
+  }
+
+
 }
