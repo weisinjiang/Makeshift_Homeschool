28d7681da0749f00ea59446f4ebb0c4d2db6c8e3
sumay11 <69305154+sumay11@users.noreply.github.com>
2021-04-21 10:28:35 -0400

    started making search screen

--- makeshift_homeschool_app/lib/widgets/widgets.dart
+++ makeshift_homeschool_app/lib/widgets/widgets.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-TextStyle simpleTextStyle() {
-  return TextStyle(
-    color: Colors.white,
-  );
-}
+// TextStyle simpleTextStyle() {
+//   return TextStyle(
+//     color: Colors.white,
+//   );
+// }
 
 InputDecoration textFieldInput(String hint) {
   return InputDecoration(
@@ -14,4 +14,34 @@ InputDecoration textFieldInput(String hint) {
           UnderlineInputBorder(borderSide: BorderSide(color: Colors.blue)),
       enabledBorder:
           UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)));
+}
+
+TextStyle simpleTextStyle() {
+ return TextStyle(color: Colors.white);
+}
+ 
+TextStyle mediumTextStyle() {
+ return TextStyle(color: Colors.white, fontSize: 20);
+}
+ 
+TextStyle titleTextStyle() {
+ return TextStyle(
+     color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold);
+}
+ 
+TextStyle subtitleTextStyle() {
+ return TextStyle(
+   color: Colors.grey[300],
+   fontSize: 20,
+ );
+}
+ 
+AppBar mainAppBar() {
+ return AppBar(
+   backgroundColor: Colors.black,
+   title: Text(
+     "Makeshift Homeschool",
+     style: mediumTextStyle(),
+   ),
+ );
 }
