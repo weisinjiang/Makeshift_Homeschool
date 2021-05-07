7ae19286c69317f158dc9348a1c3ddbca4da0ba3
sumay11 <69305154+sumay11@users.noreply.github.com>
2021-05-07 14:58:10 -0400

    Made didChangeDepandancies

--- makeshift_homeschool_app/lib/screens/students_screen.dart
+++ makeshift_homeschool_app/lib/screens/students_screen.dart
@@ -1,17 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:makeshift_homeschool_app/services/student_page_provider.dart';
+import 'package:provider/provider.dart';
 
 QuerySnapshot studentSnapshot;
 
 class StudentsPage extends StatefulWidget {
   //^ Make StudentProvider (see post_feed_provider.dart)
 
-  //^ add didChangeDepandancies and init
+
   @override
   _StudentsPageState createState() => _StudentsPageState();
 }
 
 class _StudentsPageState extends State<StudentsPage> {
+  var _isInit = true;
+  var _isLoading = false;
+
+
+  //^ didChangeDependancies
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      // fetch all posts 
+      var studentProvider = Provider.of<StudentsPageProvider>(context);
+      studentProvider.fetchUsers().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
